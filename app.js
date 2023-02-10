fe04c640706ac74f720dd784d600c4a6185c4981
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-10 22:18:14 +0000

    Added comments

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// set up express object
 const express = require('express')
 const app = express()
 const port = 3000
 
+// respond to HTTP GET request with string
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// listen on port 3000 for requests
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
